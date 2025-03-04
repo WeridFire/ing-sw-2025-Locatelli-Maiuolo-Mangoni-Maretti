dfd2932c7bdd744178b94131bbabc4dc255a0f42
Maretti Filippo <69721649+WeridFire@users.noreply.github.com>
2025-03-04 16:08:33 +0100

    Delete src/main directory
    restart

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.fmad.galaxytrucker {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens com.fmad.galaxytrucker to javafx.fxml;
-    exports com.fmad.galaxytrucker;
-}
