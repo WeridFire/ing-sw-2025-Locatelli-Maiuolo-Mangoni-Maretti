b31f39fb6682a4f70200643751412436c04f14fe
Manuel Maiuolo <manuelmaiuolo@gmail.com>
2025-03-14 13:53:52 +0100

    refactor: LoadableItems not anymore as bitwise enum -> simple enums in Battery, Cargo and Crew types; no need for IFlagEnum and GameData.availableGoods is now CargoType

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/game/GameData.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/game/GameData.java
@@ -1,10 +1,11 @@
 package src.main.java.it.polimi.ingsw.game;
 
+import src.main.java.it.polimi.ingsw.enums.CargoType;
 import src.main.java.it.polimi.ingsw.enums.GameLevel;
 import src.main.java.it.polimi.ingsw.enums.GamePhaseType;
-import src.main.java.it.polimi.ingsw.enums.LoadableItem;
 import src.main.java.it.polimi.ingsw.gamePhases.PlayableGamePhase;
 import src.main.java.it.polimi.ingsw.shipboard.ShipBoard;
+import src.main.java.it.polimi.ingsw.shipboard.tiles.ILoadableItem;
 import src.main.java.it.polimi.ingsw.shipboard.tiles.Tile;
 
 import java.util.ArrayList;
@@ -25,7 +26,7 @@ public class GameData {
 
     //private HashMap<Player, ShipBoard> shipFromPlayer;
 
-    private HashMap<LoadableItem, Integer> availableGoods;
+    private HashMap<CargoType, Integer> availableGoods;
 
     private int availableCredits;
 
