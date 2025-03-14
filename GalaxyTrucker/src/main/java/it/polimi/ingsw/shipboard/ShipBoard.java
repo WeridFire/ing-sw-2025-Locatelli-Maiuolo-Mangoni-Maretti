40edf19f649285c44f5cebe32201ca69e67b7c07
Manuel Maiuolo <manuelmaiuolo@gmail.com>
2025-03-14 01:20:47 +0100

    feat(shipboard): Main to test test-Tiles

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/shipboard/ShipBoard.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/shipboard/ShipBoard.java
@@ -1,4 +1,32 @@
 package src.main.java.it.polimi.ingsw.shipboard;
 
+import src.main.java.it.polimi.ingsw.enums.Rotation;
+import src.main.java.it.polimi.ingsw.shipboard.tiles.Tile;
+
+import java.util.Arrays;
+
 public class ShipBoard {
+
+    public static void main(String[] args) {
+        /* TEST
+        Tile[] tiles = new Tile[] {
+                new Tile("E", "N", "W", "S", 1),
+                new Tile("E", "N", "W", "S", 2),
+                new Tile("E", "N", "W", "S", 3),
+                new Tile("Ea", "Na", "Wa", "Sa", 4),
+                new Tile("E", "N", "W", "S", 5)
+        };
+        System.out.println(Arrays.toString(tiles));
+        tiles[2].rotateTile(Rotation.COUNTERCLOCKWISE);
+        System.out.println(Arrays.toString(tiles));
+        tiles[3].rotateTile(Rotation.OPPOSITE);
+        tiles[3].rotateTile(Rotation.CLOCKWISE);
+        tiles[3].rotateTile(Rotation.OPPOSITE);
+        System.out.println(Arrays.toString(tiles));
+        for (Tile tile : tiles) {
+            tile.resetRotation();
+            System.out.println(tile);
+        }
+        */
+    }
 }
