76d6c18728120c3937d3ec212879a45670e8671b
Maretti Filippo <69721649+WeridFire@users.noreply.github.com>
2025-03-15 17:44:05 +0100

    Built skeleton of PlayableGamePhase.java

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/gamePhases/PlayableGamePhase.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/gamePhases/PlayableGamePhase.java
@@ -1,4 +1,56 @@
 package src.main.java.it.polimi.ingsw.gamePhases;
 
+import src.main.java.it.polimi.ingsw.enums.GamePhaseType;
+import src.main.java.it.polimi.ingsw.enums.GameState;
+import src.main.java.it.polimi.ingsw.game.GameData;
+
+import java.util.UUID;
+
+/**
+ * Represents an abstract playable game phase.
+ * Defines the structure for different game phases that can be played.
+ */
 public abstract class PlayableGamePhase {
+
+    /** The type of the game phase. */
+    GamePhaseType gamePhaseType;
+
+    /** The current state of the game. */
+    GameState gameState;
+
+    /** The unique identifier of the game. */
+    UUID gameId;
+
+    /** The game data associated with this phase. */
+    GameData gameData;
+
+    /**
+     * Constructs a new PlayableGamePhase.
+     *
+     * @param gameId The unique identifier of the game.
+     * @param gamePhaseType The type of the game phase.
+     * @param gameState The current state of the game.
+     * @param gameData The game data.
+     */
+    public PlayableGamePhase(UUID gameId, GamePhaseType gamePhaseType, GameState gameState, GameData gameData) {
+        this.gameId = gameId;
+        this.gamePhaseType = gamePhaseType;
+        this.gameState = gameState;
+        this.gameData = gameData;
+    }
+
+    /**
+     * Gets the type of the game phase.
+     *
+     * @return The game phase type.
+     */
+    public GamePhaseType getGamePhaseType() {
+        return gamePhaseType;
+    }
+
+    /**
+     * Defines the main gameplay loop for this phase.
+     * To be implemented by subclasses.
+     */
+    public void playLoop(){}
 }
