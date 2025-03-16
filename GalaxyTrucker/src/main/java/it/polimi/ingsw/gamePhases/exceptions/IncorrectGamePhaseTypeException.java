ab6ad36d194e330196082aee9918c2b5b9de7b1d
Maretti Filippo <69721649+WeridFire@users.noreply.github.com>
2025-03-16 17:13:57 +0100

    Useful exception for PlayableGamePhase

---
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/gamePhases/exceptions/IncorrectGamePhaseTypeException.java
@@ -0,0 +1,7 @@
+package src.main.java.it.polimi.ingsw.gamePhases.exceptions;
+
+public class IncorrectGamePhaseTypeException extends RuntimeException {
+    public IncorrectGamePhaseTypeException(String message) {
+        super(message);
+    }
+}
---
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/gamePhases/exceptions/NoMoreTimerResetLeftException.java
@@ -0,0 +1,5 @@
+package src.main.java.it.polimi.ingsw.gamePhases.exceptions;
+
+public class NoMoreTimerResetLeftException extends RuntimeException{
+    public NoMoreTimerResetLeftException(String message) {super(message);}
+}
