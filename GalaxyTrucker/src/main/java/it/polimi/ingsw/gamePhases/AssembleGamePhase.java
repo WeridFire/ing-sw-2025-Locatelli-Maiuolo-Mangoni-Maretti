26520d84c05624604a1d69cacd450b5f8f4eee56
Maretti Filippo <69721649+WeridFire@users.noreply.github.com>
2025-03-16 17:14:31 +0100

    Fist AssembleGamePhase skeleton

---
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/gamePhases/AssembleGamePhase.java
@@ -0,0 +1,77 @@
+package src.main.java.it.polimi.ingsw.gamePhases;
+
+import src.main.java.it.polimi.ingsw.GamesHandler;
+import src.main.java.it.polimi.ingsw.enums.GameLevel;
+import src.main.java.it.polimi.ingsw.enums.GamePhaseType;
+import src.main.java.it.polimi.ingsw.enums.GameState;
+import src.main.java.it.polimi.ingsw.game.GameData;
+import src.main.java.it.polimi.ingsw.gamePhases.exceptions.IncorrectGamePhaseTypeException;
+import src.main.java.it.polimi.ingsw.gamePhases.exceptions.NoMoreTimerResetLeftException;
+import src.main.java.it.polimi.ingsw.timer.Timer;
+
+import java.util.UUID;
+
+public class AssembleGamePhase extends PlayableGamePhase{
+
+    private int howManyTimerRotationsLeft;
+
+    private final Timer timer;
+
+    /**
+     * Constructs a new PlayableGamePhase.
+     *
+     * @param gameId        The unique identifier of the game.
+     * @param gamePhaseType The type of the game phase.
+     * @param gameState     The current state of the game.
+     * @param gameData      The game data.
+     */
+    public AssembleGamePhase(UUID gameId, GamePhaseType gamePhaseType, GameState gameState, GameData gameData) {
+        super(gameId, gamePhaseType, gameState, gameData);
+
+        if (!gamePhaseType.equals(GamePhaseType.ASSEMBLE)){
+            throw new IncorrectGamePhaseTypeException("Assemble gamePhase type is not coherent");
+        }
+
+        if (gameData.getLevel().equals(GameLevel.TESTFLIGHT)){
+            this.howManyTimerRotationsLeft = -1;
+        } else if (gameData.getLevel().equals(GameLevel.TWO)){
+            this.howManyTimerRotationsLeft = 2;
+        }
+
+        this.timer = GamesHandler.getInstance().getGame(gameId).getTimer();
+    }
+
+    @Override
+    public void playLoop() {
+        if (howManyTimerRotationsLeft < 0){
+            // testflight logic
+            return;
+        }
+        // level two game logic
+        timer.start();
+
+    }
+
+    @Override
+    public void onTick() {
+        //TBD some JavaFX things probably
+    }
+
+    @Override
+    public void onTimerEnd() {
+        if (howManyTimerRotationsLeft == 0){
+            gameState = GameState.ENDED;
+            return;
+        }
+        howManyTimerRotationsLeft--;
+    }
+
+    private void resetTimer() {
+        if (howManyTimerRotationsLeft < 0){
+            throw new NoMoreTimerResetLeftException("No more timer reset");
+        }
+        howManyTimerRotationsLeft--;
+        timer.restart();
+    }
+
+}
