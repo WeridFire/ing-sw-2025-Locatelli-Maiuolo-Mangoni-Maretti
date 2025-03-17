3187d223337808cf7ffd44bc7e50fe0e90b2a2ed
Dere-Wah <derexcontact@gmail.com>
2025-03-17 15:29:19 +0100

    feat(Planet): implement planet behaviour

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/cards/planets/Planet.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/cards/planets/Planet.java
@@ -1,8 +1,41 @@
 package src.main.java.it.polimi.ingsw.cards.planets;
 
+import src.main.java.it.polimi.ingsw.enums.CargoType;
+import src.main.java.it.polimi.ingsw.player.Player;
+
+import java.util.List;
+
 public class Planet {
+	/**
+	 * Dynamic list of the currently available goods on the planet.
+	 */
+	private List<CargoType> availableGoods;
 
+	/**
+	 * The player currently landed on the planet.
+	 */
+	private Player currentPlayer;
 
+	/**
+	 * Instances a planet for the PlanetsCard.
+	 * @param availableGoods The goods available on the planet.
+	 */
+	public Planet(List<CargoType> availableGoods){
+		this.availableGoods = availableGoods;
+	}
 
+	/**
+	 *
+	 * @ the dynamic list of the currently available goods on the planet.
+	 */
+	public List<CargoType> getAvailableGoods() {
+		return availableGoods;
+	}
 
+	public void assignGood(CargoType good){
+		if(availableGoods.contains(good)){
+			availableGoods.remove(good);
+			//TODO: Ask player the Tile where to put the good on. Add the good in the correct position on the ship.
+		}
+	}
 }
