4f43ef82e737896cbbe8f961598800849d648324
Manuel Maiuolo <manuelmaiuolo@gmail.com>
2025-03-19 23:15:24 +0100

    refactor(shipboard): duplicated tiles' exceptions from old shipboard (shipboard1) to current shipboard

---
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/shipboard/tiles/exceptions/UnsupportedLoadableItemException.java
@@ -0,0 +1,7 @@
+package src.main.java.it.polimi.ingsw.shipboard.tiles.exceptions;
+
+public class UnsupportedLoadableItemException extends Exception {
+    public UnsupportedLoadableItemException(String message) {
+        super(message);
+    }
+}
