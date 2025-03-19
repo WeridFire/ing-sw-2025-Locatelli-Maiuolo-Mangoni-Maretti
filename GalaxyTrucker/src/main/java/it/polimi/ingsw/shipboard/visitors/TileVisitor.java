0614888fdbcf1f1b1daedb0895e7d45f6f9189a6
Manuel Maiuolo <manuelmaiuolo@gmail.com>
2025-03-19 23:19:34 +0100

    feat(shipboard): updated TileVisitor with new elements (tiles) to visit

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/shipboard/visitors/TileVisitor.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/shipboard/visitors/TileVisitor.java
@@ -7,9 +7,23 @@ public interface TileVisitor {
     /**
      * What to do when visiting the provided structural tile.
      * To be implemented in each visitor.
-     * @param structural The visited structural tile.
+     * @param tile The visited structural tile.
      */
-    void visitStructural(StructuralTile structural);
+    void visitStructural(StructuralTile tile);
+
+    /**
+     * What to do when visiting the provided life support system tile.
+     * To be implemented in each visitor.
+     * @param tile The visited life support system tile.
+     */
+    void visitLifeSupportSystem(LifeSupportSystemTile tile);
+
+    /**
+     * What to do when visiting the provided cargo hold tile.
+     * To be implemented in each visitor.
+     * @param tile The visited cargo hold tile.
+     */
+    void visitCargoHold(CargoHoldTile tile);
 
     // TODO: add all the other concrete Tiles
     // example -> void visitEngine(EngineTile engine); ...
