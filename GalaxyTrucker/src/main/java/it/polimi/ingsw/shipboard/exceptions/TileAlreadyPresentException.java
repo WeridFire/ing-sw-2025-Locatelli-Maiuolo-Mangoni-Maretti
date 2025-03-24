aa36379adb565dcbbcbc71bc81000340641aa54d
Manuel Maiuolo <manuelmaiuolo@gmail.com>
2025-03-24 23:29:10 +0100

    refactor: duplicated useful exceptions from old shipboard to current

---
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/shipboard/exceptions/TileAlreadyPresentException.java
@@ -0,0 +1,14 @@
+package src.main.java.it.polimi.ingsw.shipboard.exceptions;
+
+import src.main.java.it.polimi.ingsw.shipboard1.tiles.Tile;
+import src.main.java.it.polimi.ingsw.util.Coordinates;
+
+public class TileAlreadyPresentException extends Exception {
+    public TileAlreadyPresentException(String message) {
+        super(message);
+    }
+
+    public TileAlreadyPresentException(Coordinates coordinates, Tile tile) {
+        super("Tile " + tile + " already present at coordinates " + coordinates);
+    }
+}
