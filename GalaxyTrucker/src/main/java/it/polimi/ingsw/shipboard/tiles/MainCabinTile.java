84a28a378da8cd5898329a96d2e90b152591b01e
Manuel Maiuolo <manuelmaiuolo@gmail.com>
2025-03-24 23:29:41 +0100

    feat(shipboard>visitors): can now distinguish between normal cabin and main cabin with visitor pattern

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/shipboard/tiles/MainCabinTile.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/shipboard/tiles/MainCabinTile.java
@@ -3,6 +3,7 @@ package src.main.java.it.polimi.ingsw.shipboard.tiles;
 import src.main.java.it.polimi.ingsw.enums.Direction;
 import src.main.java.it.polimi.ingsw.shipboard.LoadableType;
 import src.main.java.it.polimi.ingsw.shipboard.SideType;
+import src.main.java.it.polimi.ingsw.shipboard.visitors.TileVisitor;
 
 import java.util.Set;
 
@@ -15,5 +16,10 @@ public class MainCabinTile extends CabinTile {
         super(Direction.sortedArray(SideType.UNIVERSAL, SideType.UNIVERSAL,
                         SideType.UNIVERSAL, SideType.UNIVERSAL).toArray(new SideType[0]),
                 Set.of(LoadableType.HUMAN));
+    }
+
+    @Override
+    public void accept(TileVisitor visitor) {
+        visitor.visitMainCabin(this);
     }
 }
