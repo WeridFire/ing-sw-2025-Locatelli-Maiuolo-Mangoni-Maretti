472b47d29144479171431a74ed55ab1d61d84af0
Dere-Wah <derexcontact@gmail.com>
2025-03-30 21:54:02 +0200

    feat(PlayerTurnType): initialize POPULATE_TILE

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/playerInput/PlayerTurnType.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/playerInput/PlayerTurnType.java
@@ -2,6 +2,6 @@ package src.main.java.it.polimi.ingsw.playerInput;
 
 public enum PlayerTurnType {
 
-	ADD_CARGO, REMOVE_CARGO, ACTIVATE_TILE
+	ADD_CARGO, REMOVE_CARGO, ACTIVATE_TILE, POPULATE_TILES
 
 }
