5ced16fb0ffc69a9dba2b6a13967976921117373
Dere-Wah <derexcontact@gmail.com>
2025-03-31 13:54:17 +0200

    docs(PlayerActivateTilesRequest): add docs

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/playerInput/PlayerActivateTilesRequest.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/playerInput/PlayerActivateTilesRequest.java
@@ -57,6 +57,17 @@ public class PlayerActivateTilesRequest extends PlayerInputRequest {
 				.count(LoadableType.BATTERY) - getActivatedTiles().size());
 	}
 
+	/**
+	 * This input action will allow the player to specify which coordinates they want to activate. It can only
+	 * be used for power types of type PowerType.FIRE & PowerType.THRUST. Once the player submits the action, the
+	 * turn will auto end. If the player submits a list of coordinates that are not valid, the game will notify an
+	 * error but will not end the turn.
+	 * @param player The player that sent the command
+	 * @param coordinates a set of tile coordinates to activate
+	 * @throws WrongPlayerTurnException If the player requesting the command is not who the turn is for.
+	 * @throws NotEnoughItemsException If the player does not have enough batteries to activate the tiles.
+	 * @throws TileNotAvailableException If the tile is not supported for this action in this turn.
+	 */
 	@Override
 	public void activateTiles(Player player, Set<Coordinates> coordinates) throws WrongPlayerTurnException, NotEnoughItemsException, TileNotAvailableException {
 		checkForTurn(player);
@@ -65,10 +76,10 @@ public class PlayerActivateTilesRequest extends PlayerInputRequest {
 		}
 		if (getAvailableBatteriesAmount() >= coordinates.size()) {
 			activatedTiles.addAll(coordinates);
+			endTurn();
 		} else {
 			throw new NotEnoughItemsException("Attempt to activate more tiles than batteries available");
 		}
-		endTurn();
 	}
 
 	public Set<Coordinates> getActivatedTiles() {
