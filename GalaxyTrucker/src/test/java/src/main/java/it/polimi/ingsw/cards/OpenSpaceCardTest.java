049996b17ac60412f2fa7fbbc0b0579375ecba85
AdriCman <adrianoclaudio.mangoni@mail.polimi.it>
2025-04-01 13:30:43 +0200

    feat(test): first tests on Cards

---
+++ GalaxyTrucker/src/test/java/src/main/java/it/polimi/ingsw/cards/OpenSpaceCardTest.java
@@ -0,0 +1,12 @@
+package src.main.java.it.polimi.ingsw.cards;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OpenSpaceCardTest {
+
+    @Test
+    void playEffect() {
+    }
+}
