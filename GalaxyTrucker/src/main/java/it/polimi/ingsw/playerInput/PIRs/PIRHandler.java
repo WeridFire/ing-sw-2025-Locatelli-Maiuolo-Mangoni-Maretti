bdb4c0685a08423a24d7fa67828e113ce31df789
Dere-Wah <derexcontact@gmail.com>
2025-04-03 17:13:27 +0200

    fix: make serializable

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/playerInput/PIRs/PIRHandler.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/playerInput/PIRs/PIRHandler.java
@@ -6,9 +6,10 @@ import it.polimi.ingsw.playerInput.exceptions.InputNotSupportedException;
 import it.polimi.ingsw.playerInput.exceptions.WrongPlayerTurnException;
 import it.polimi.ingsw.util.Coordinates;
 
+import java.io.Serializable;
 import java.util.Set;
 
-public class PIRHandler {
+public class PIRHandler implements Serializable {
 
 	private PIRActivateTiles activateTiles = null;
 	private PIRAddLoadables addLoadables = null;
