780824a83df173c7ee42e652c5e94519c94af50c
Dere-Wah <derexcontact@gmail.com>
2025-04-03 14:17:40 +0200

    fix(Assemble): make exception not runtime

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/gamePhases/exceptions/TimerIsAlreadyRunningException.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/gamePhases/exceptions/TimerIsAlreadyRunningException.java
@@ -1,6 +1,6 @@
 package it.polimi.ingsw.gamePhases.exceptions;
 
-public class TimerIsAlreadyRunningException extends RuntimeException {
+public class TimerIsAlreadyRunningException extends Exception {
     public TimerIsAlreadyRunningException(String message) {
         super(message);
     }
