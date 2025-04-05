bbb3dbca3bce56b98493b96e812bb0b232711f8d
AdriCman <adrianoclaudio.mangoni@mail.polimi.it>
2025-04-05 22:51:48 +0200

    feat(PlayerTest): drawTileTest

--- GalaxyTrucker/src/test/java/it/polimi/ingsw/player/PlayerTest.java
+++ GalaxyTrucker/src/test/java/it/polimi/ingsw/player/PlayerTest.java
@@ -1,8 +1,13 @@
 package it.polimi.ingsw.player;
 
 import it.polimi.ingsw.TilesFactory;
+import it.polimi.ingsw.cards.Deck;
 import it.polimi.ingsw.enums.Direction;
 import it.polimi.ingsw.enums.GameLevel;
+import it.polimi.ingsw.enums.GamePhaseType;
+import it.polimi.ingsw.game.GameData;
+import it.polimi.ingsw.game.exceptions.DrawTileException;
+import it.polimi.ingsw.player.exceptions.AlreadyHaveTileInHandException;
 import it.polimi.ingsw.shipboard.ShipBoard;
 import it.polimi.ingsw.shipboard.SideType;
 import it.polimi.ingsw.shipboard.exceptions.OutOfBuildingAreaException;
@@ -10,18 +15,29 @@ import it.polimi.ingsw.shipboard.exceptions.TileAlreadyPresentException;
 import it.polimi.ingsw.shipboard.tiles.CabinTile;
 import it.polimi.ingsw.shipboard.tiles.TileSkeleton;
 import it.polimi.ingsw.shipboard.tiles.exceptions.FixedTileException;
+import it.polimi.ingsw.shipboard.visitors.TileVisitor;
 import it.polimi.ingsw.util.Coordinates;
+import it.polimi.ingsw.view.cli.CLIFrame;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.engine.TestDescriptor;
+
+import static it.polimi.ingsw.enums.GamePhaseType.ASSEMBLE;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
 class PlayerTest {
 
-    Player player1 = new Player("SpaceTruckKing", UUID.randomUUID());
-    ShipBoard shipBoard1 = new ShipBoard(GameLevel.TWO);
+    private GameData gameData;
+    private UUID gameId;
+    private Player player1;
+    private ShipBoard shipBoard1;
+    private GamePhaseType gamePhaseType;
+
     SideType[] sideCannon1 = new SideType[4];
     Coordinates cabinCord1 = new Coordinates(5, 6);
     CabinTile cabin1 = new CabinTile(Direction.sortedArray(
@@ -29,6 +45,28 @@ class PlayerTest {
     Coordinates cabinCord2 = new Coordinates(6, 6);
     CabinTile cabin2 = new CabinTile(Direction.sortedArray(
             SideType.SINGLE, SideType.UNIVERSAL, SideType.UNIVERSAL, SideType.SMOOTH).toArray(SideType[]::new));
+
+    @BeforeEach
+    void setUp() {
+        player1 = new Player("SpaceTruckKing", UUID.randomUUID());
+        shipBoard1 = new ShipBoard(GameLevel.TWO);
+        gameId = UUID.randomUUID();
+        gameData = new GameData(gameId);
+    }
+
+    @Test
+    void testDrawTile() throws DrawTileException, AlreadyHaveTileInHandException {
+
+        List<TileSkeleton> MockTiles = new ArrayList<>();
+        TileSkeleton mockTile1 = createMockTile(1);
+        TileSkeleton mockTile2 = createMockTile(2);
+        MockTiles.add(mockTile1);
+        MockTiles.add(mockTile2);
+        gameData.setCoveredTiles(MockTiles);
+        gameData.setCurrentGamePhaseType(ASSEMBLE);
+        player1.drawTile(gameData);
+        assertNotNull(player1.getTileInHand());
+    }
 
 
     @Test
@@ -57,5 +95,28 @@ class PlayerTest {
         assertDoesNotThrow(() -> player1.getShipBoard().endAssembly());
         System.out.println("\nTest AFTER assembly's ended\n");
         System.out.println(player1.getShipBoard().getCLIRepresentation());
+    }
+
+    private TileSkeleton createMockTile(int id) {
+        return new TileSkeleton(null) {
+            /**
+             * Method to accept a visitor for this tile in the Visitor Pattern.
+             *
+             * @param visitor The visitor that will visit this tile.
+             */
+            @Override
+            public void accept(TileVisitor visitor) {
+
+            }
+
+            @Override
+            public CLIFrame getCLIRepresentation() {
+                return null;
+            }
+
+            {
+                setTileId(id);
+            }
+        };
     }
 }
