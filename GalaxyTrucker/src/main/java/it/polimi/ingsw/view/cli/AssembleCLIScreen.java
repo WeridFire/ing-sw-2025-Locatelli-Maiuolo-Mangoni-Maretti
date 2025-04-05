d4920cb9cd71c447feb37261e720b3aea757bda1
Maretti Filippo <69721649+WeridFire@users.noreply.github.com>
2025-04-05 11:01:51 +0200

    drawTile, fliphourglass CLI implementation. Still need for an assemblephase cli

---
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/view/cli/AssembleCLIScreen.java
@@ -0,0 +1,47 @@
+package it.polimi.ingsw.view.cli;
+
+import it.polimi.ingsw.enums.GamePhaseType;
+
+import java.rmi.RemoteException;
+
+public class AssembleCLIScreen extends CLIScreen{
+    public AssembleCLIScreen(String screenName, boolean forceActivate) {
+        super(screenName, forceActivate);
+    }
+
+    public AssembleCLIScreen(String screenName) {
+        super(screenName);
+    }
+
+    @Override
+    protected boolean switchConditions() {
+        return getLastUpdate().getCurrentGame() != null &&
+                getLastUpdate().getCurrentGame().getCurrentGamePhaseType() == GamePhaseType.ASSEMBLE;
+    }
+
+    @Override
+    protected void processCommand(String command, String[] args) throws RemoteException, IllegalArgumentException {
+        command = command.toLowerCase();
+        switch(command){
+            case "timerflip":
+                getServer().flipHourglass(getClient());
+                break;
+
+            case "drawtile":
+                getServer().drawTile(getClient());
+        }
+    }
+
+    @Override
+    void printScreenSpecificCommands() {
+        printCommands(screenName,
+                "timerflip|Flips the hourglass of the game."
+        );
+    }
+
+    @Override
+    public CLIFrame getCLIRepresentation() {
+        //TODO: think about the assemble game screen
+        return null;
+    }
+}
