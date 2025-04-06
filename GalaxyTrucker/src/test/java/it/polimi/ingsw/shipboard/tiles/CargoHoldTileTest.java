019fe51cf0bdd96ef68e239f4aa50971872db2f0
AdriCman <adrianoclaudio.mangoni@mail.polimi.it>
2025-04-06 17:17:05 +0200

    feat(CargoHoldTileTest): Test

---
+++ GalaxyTrucker/src/test/java/it/polimi/ingsw/shipboard/tiles/CargoHoldTileTest.java
@@ -0,0 +1,63 @@
+package it.polimi.ingsw.shipboard.tiles;
+
+import it.polimi.ingsw.enums.Direction;
+import it.polimi.ingsw.shipboard.SideType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CargoHoldTileTest {
+
+    private CargoHoldTile regularDoubleCargoHoldTile;
+    private CargoHoldTile regularTripleCargoHoldTile;
+    private CargoHoldTile specialSingleCargoHoldTile;
+    private CargoHoldTile specialDoubleCargoHoldTile;
+
+    @BeforeEach
+    void setUp() {
+        SideType[] regularDoubleCargo = Direction.sortedArray(
+                SideType.UNIVERSAL,  // NORTH
+                SideType.UNIVERSAL,  // EAST
+                SideType.CANNON,     // SOUTH
+                SideType.UNIVERSAL   // WEST
+        ).toArray(SideType[]::new);
+
+        regularDoubleCargoHoldTile = new CargoHoldTile(regularDoubleCargo, 2);
+
+        SideType[] regularTripleCargo = Direction.sortedArray(
+                SideType.UNIVERSAL,  // NORTH
+                SideType.UNIVERSAL,  // EAST
+                SideType.CANNON,     // SOUTH
+                SideType.UNIVERSAL   // WEST
+        ).toArray(SideType[]::new);
+
+        regularTripleCargoHoldTile = new CargoHoldTile(regularTripleCargo, 3);
+
+        SideType[] specialSingleCargo = Direction.sortedArray(
+                SideType.UNIVERSAL,  // NORTH
+                SideType.UNIVERSAL,  // EAST
+                SideType.CANNON,     // SOUTH
+                SideType.UNIVERSAL   // WEST
+        ).toArray(SideType[]::new);
+
+        specialSingleCargoHoldTile = new CargoHoldTile(specialSingleCargo, 1);
+
+        SideType[] specialDoubleCargo = Direction.sortedArray(
+                SideType.UNIVERSAL,  // NORTH
+                SideType.UNIVERSAL,  // EAST
+                SideType.CANNON,     // SOUTH
+                SideType.UNIVERSAL   // WEST
+        ).toArray(SideType[]::new);
+
+        specialDoubleCargoHoldTile = new CargoHoldTile(specialDoubleCargo, 2);
+
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals("2+", regularDoubleCargoHoldTile.getCLISymbol());
+        assertEquals("3+", regularTripleCargoHoldTile.getCLISymbol());
+    }
+
+}
