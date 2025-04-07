3a161f6879cb63653e4d9b26260852a7a80f766d
AdriCman <adrianoclaudio.mangoni@mail.polimi.it>
2025-04-07 22:32:31 +0200

    feat(EngineTileTest): VisitorTest

--- GalaxyTrucker/src/test/java/it/polimi/ingsw/shipboard/tiles/EngineTileTest.java
+++ GalaxyTrucker/src/test/java/it/polimi/ingsw/shipboard/tiles/EngineTileTest.java
@@ -4,6 +4,7 @@ import it.polimi.ingsw.enums.Direction;
 import it.polimi.ingsw.shipboard.SideType;
 import it.polimi.ingsw.shipboard.tiles.exceptions.FixedTileException;
 import it.polimi.ingsw.shipboard.tiles.exceptions.NotFixedTileException;
+import it.polimi.ingsw.shipboard.visitors.TileVisitor;
 import it.polimi.ingsw.util.Coordinates;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -81,5 +82,77 @@ class EngineTileTest {
 
         // Test that you can't place the tile again
         assertThrows(FixedTileException.class, () -> singleEngineTile.place(new Coordinates(5, 6)));
+    }
+
+    @Test
+    void testGetCoordinates() {
+
+        // Test that getCoordinates throws NotFixedTileException if the tile has not been placed
+        assertThrows(NotFixedTileException.class, () -> singleEngineTile.getCoordinates());
+    }
+
+    @Test
+    void testCLIRepresentation() {
+
+        // Test that the tile returns a non-null CLI representation
+        assertNotNull(singleEngineTile.getCLIRepresentation());
+        assertNotNull(doubleEngineTile.getCLIRepresentation());
+    }
+
+    @Test
+    void testAccept() {
+
+        // Create a stub for TileVisitor
+        EngineTileTest.TestTileVisitor visitor = new EngineTileTest.TestTileVisitor();
+
+        // Call accept on the cannon tile
+        singleEngineTile.accept(visitor);
+
+        // Verify that visitCannon was called
+        assertTrue(visitor.visitEngineCalled);
+        assertEquals(singleEngineTile, visitor.lastEngineVisited);
+    }
+
+    // Helper class to test the visitor pattern
+    private static class TestTileVisitor implements TileVisitor {
+        boolean visitEngineCalled = false;
+        EngineTile lastEngineVisited = null;
+
+        @Override
+        public void visitEngine(EngineTile tile) {
+            visitEngineCalled = true;
+            lastEngineVisited = tile;
+        }
+
+        // Implement other required methods with empty bodies
+        @Override public void visitStructural(StructuralTile tile) {}
+
+        /**
+         * What to do when visiting the provided life support system tile.
+         * To be implemented in each visitor.
+         *
+         * @param tile The visited life support system tile.
+         */
+        @Override
+        public void visitLifeSupportSystem(LifeSupportSystemTile tile) {}
+
+        @Override
+        public void visitCabin(CabinTile tile) {}
+
+        @Override
+        public void visitMainCabin(CabinTile tile) {}
+
+        @Override
+        public void visitBatteryComponent(BatteryComponentTile tile) {}
+
+        @Override
+        public void visitCannon(CannonTile tile) {}
+
+        @Override
+        public void visitShieldGenerator(ShieldGeneratorTile tile) {}
+
+        @Override
+        public void visitCargoHold(CargoHoldTile tile) {}
+
     }
 }
