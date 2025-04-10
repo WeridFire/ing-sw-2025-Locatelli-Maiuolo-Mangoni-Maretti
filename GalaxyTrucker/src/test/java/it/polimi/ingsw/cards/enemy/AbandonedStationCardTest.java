77ea51daf71b576530cde2055ee7f4ddc1aefd8b
Maretti Filippo <69721649+WeridFire@users.noreply.github.com>
2025-04-10 17:34:10 +0200

    test: getCLIRep

---
+++ GalaxyTrucker/src/test/java/it/polimi/ingsw/cards/enemy/AbandonedStationCardTest.java
@@ -0,0 +1,31 @@
+package it.polimi.ingsw.cards.enemy;
+
+import it.polimi.ingsw.cards.AbandonedShipCard;
+import it.polimi.ingsw.cards.AbandonedStationCard;
+import it.polimi.ingsw.shipboard.LoadableType;
+import it.polimi.ingsw.view.cli.CLIFrame;
+import org.junit.jupiter.api.Test;
+
+public class AbandonedStationCardTest {
+    @Test
+    void testGetCLIRepresentation() {
+        // Create a pirate card with specific properties for testing visualization
+        AbandonedStationCard testCard = new AbandonedStationCard(
+                new LoadableType[] {
+                        LoadableType.RED_GOODS,
+                        LoadableType.BLUE_GOODS,
+                        LoadableType.GREEN_GOODS,
+                        LoadableType.YELLOW_GOODS
+                },
+                2,
+                3,
+                "",
+                3
+        );
+
+        // Get the CLI representation
+        CLIFrame frame = testCard.getCLIRepresentation();
+
+        System.out.println(frame);
+    }
+}
