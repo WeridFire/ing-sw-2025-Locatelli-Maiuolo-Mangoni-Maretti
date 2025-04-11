ac1d62100e9b71c7d278b65563b128fee72d04dd
Dere-Wah <derexcontact@gmail.com>
2025-04-11 16:03:19 +0200

    feat(endAssembly): add check for current game phase, update method signature to accept game for this check.

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/shipboard/ShipBoard.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/shipboard/ShipBoard.java
@@ -1,6 +1,8 @@
 package it.polimi.ingsw.shipboard;
 
 import it.polimi.ingsw.enums.*;
+import it.polimi.ingsw.game.GameData;
+import it.polimi.ingsw.gamePhases.exceptions.CommandNotAllowedException;
 import it.polimi.ingsw.shipboard.tiles.*;
 import it.polimi.ingsw.shipboard.tiles.exceptions.FixedTileException;
 import it.polimi.ingsw.shipboard.visitors.*;
@@ -166,7 +168,11 @@ public class ShipBoard implements ICLIPrintable, Serializable {
 	 * Prepares the ship for flight.
 	 * @implNote Resets all visitors (also integrity check is called).
 	 */
-	public void endAssembly() throws AlreadyEndedAssemblyException {
+	public void endAssembly(GameData gameData) throws AlreadyEndedAssemblyException, CommandNotAllowedException {
+		if(gameData.getCurrentGamePhaseType() != GamePhaseType.ASSEMBLE){
+			throw new CommandNotAllowedException("end assembly", "The game is not in assembly phase.");
+		}
+
 		if (endedAssembly) {
 			throw new AlreadyEndedAssemblyException();
 		}
