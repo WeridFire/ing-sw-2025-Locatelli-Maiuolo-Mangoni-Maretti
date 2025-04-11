cc4a2b403edb16a97151ced6b6f71debdbdc2822
Maretti Filippo <69721649+WeridFire@users.noreply.github.com>
2025-04-11 17:06:56 +0200

    test: StardustCardTest

---
+++ GalaxyTrucker/src/test/java/it/polimi/ingsw/cards/enemy/StardustCardTest.java
@@ -0,0 +1,22 @@
+package it.polimi.ingsw.cards.enemy;
+
+import it.polimi.ingsw.cards.AbandonedShipCard;
+import it.polimi.ingsw.cards.StarDustCard;
+import it.polimi.ingsw.view.cli.CLIFrame;
+import org.junit.jupiter.api.Test;
+
+public class StardustCardTest {
+    @Test
+    void testGetCLIRepresentation() {
+        // Create a pirate card with specific properties for testing visualization
+        StarDustCard testcard = new StarDustCard(
+                "",
+                2
+        );
+
+        // Get the CLI representation
+        CLIFrame frame = testcard.getCLIRepresentation();
+
+        System.out.println(frame);
+    }
+}
