97e894fd563ddfeeb1201bc6199c1abafa69e9cc
Maretti Filippo <69721649+WeridFire@users.noreply.github.com>
2025-04-11 17:05:51 +0200

    feat: StarDustCard getCLIrep

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/cards/StarDustCard.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/cards/StarDustCard.java
@@ -1,11 +1,17 @@
 package it.polimi.ingsw.cards;
 
 import it.polimi.ingsw.GamesHandler;
+import it.polimi.ingsw.enums.AnchorPoint;
 import it.polimi.ingsw.game.GameData;
 import it.polimi.ingsw.player.Player;
+import it.polimi.ingsw.view.cli.ANSI;
 import it.polimi.ingsw.view.cli.CLIFrame;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+
+import static it.polimi.ingsw.view.cli.CLIScreen.getScreenFrame;
 
 public class StarDustCard extends Card{
 	/**
@@ -37,7 +43,41 @@ public class StarDustCard extends Card{
 	 */
 	@Override
 	public CLIFrame getCLIRepresentation() {
-		// TODO
-		return null;
+		/**
+		 * sembrano in obliquo i bordi ma è
+		 * perche è un commento
+		 *
+		 * +--------------+
+		 * |   PIRATES    |
+		 * | lost days: x |
+		 * | firepower: x |
+		 * | bounty: x    |
+		 * |              |
+		 * | hits:        |
+		 * | ............ |
+		 * | ............ |
+		 * +--------------+
+		 * */
+
+		CLIFrame cardBorder = getScreenFrame(11, 20, ANSI.BACKGROUND_CYAN);
+
+		// frame title
+		CLIFrame title = new CLIFrame(new String[]{
+				ANSI.WHITE + "STARDUST" + ANSI.RESET
+		});
+		cardBorder = cardBorder.merge(title, AnchorPoint.TOP, AnchorPoint.CENTER, 0, 0);
+
+		List<String> cardInfoLines = new ArrayList<>();
+		cardInfoLines.add(
+				ANSI.BLACK + "    Count your   " + ANSI.RESET
+		);
+		cardInfoLines.add(
+				ANSI.BLACK + "exposed connectors!" + ANSI.RESET
+		);
+		CLIFrame infoFrame = new CLIFrame(cardInfoLines.toArray(new String[0]));
+
+		cardBorder = cardBorder.merge(infoFrame, AnchorPoint.CENTER, AnchorPoint.CENTER, 0, 0);
+
+		return cardBorder;
 	}
 }
