f101b93162680f50534b2cc8705a4d98e3cb0776
Manuel Maiuolo <manuelmaiuolo@gmail.com>
2025-04-12 01:17:40 +0200

    feat: screen implemented for PLACE_TILE and rotate tile (rotate tile is only client-side)

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/view/cli/AssembleCLIScreen.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/view/cli/AssembleCLIScreen.java
@@ -1,14 +1,20 @@
 package it.polimi.ingsw.view.cli;
 
 import it.polimi.ingsw.enums.AnchorPoint;
+import it.polimi.ingsw.enums.Direction;
 import it.polimi.ingsw.enums.GamePhaseType;
+import it.polimi.ingsw.enums.Rotation;
 import it.polimi.ingsw.shipboard.tiles.TileSkeleton;
+import it.polimi.ingsw.shipboard.tiles.exceptions.FixedTileException;
+import it.polimi.ingsw.util.Coordinates;
 
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AssembleCLIScreen extends CLIScreen{
+
+    private TileSkeleton tileInHand = null;
 
     public AssembleCLIScreen() {
         super("assemble", true);
@@ -44,6 +50,58 @@ public class AssembleCLIScreen extends CLIScreen{
                 getServer().reserveTile(getClient());
                 break;
 
+            case "rotate":
+                // Note: this command overrides the private field, not needing confirmation from the server
+                if (args.length == 1) {
+                    Rotation rotation = Rotation.fromString(args[0]);
+                    if (rotation == Rotation.NONE) {
+                        setScreenMessage("Rotation '" + args[0] + "' not recognized.\nAllowed rotations are:"
+                                + "<left|l> | <right|r> | <opposite|o>");
+                        break;
+                    }
+                    if (tileInHand == null) {
+                        setScreenMessage("You have no tile in your hand.");
+                        break;
+                    }
+                    try {
+                        tileInHand.rotateTile(rotation);
+
+                        refresh();  // only because it's client side command
+                    } catch (FixedTileException e) {
+                        setScreenMessage("The tile in your hand has already been placed.");  // should never happen
+                        break;
+                    }
+                }
+                else {
+                    setScreenMessage("Usage: rotate <direction>");
+                }
+                break;
+
+            case "place":
+                if (args.length == 2) {
+                    Coordinates coordinates;
+                    try {
+                        int row = Integer.parseInt(args[0]);
+                        int column = Integer.parseInt(args[1]);
+                        coordinates = new Coordinates(row, column);
+                    } catch (NumberFormatException e) {
+                        setScreenMessage("Invalid coordinates. Please enter valid coordinates: " +
+                                "integer number for both row and column.");
+                        break;
+                    }
+                    //TODO: client side checks, e.g. coordinates represent valid empty place on the shipboard
+                    if (tileInHand == null) {
+                        setScreenMessage("You have no tile in your hand.");
+                        break;
+                    }
+                    // note: implicit handling of rotation
+                    getServer().placeTile(getClient(), coordinates, tileInHand.getAppliedRotation());
+                }
+                else {
+                    setScreenMessage("Usage: place <row> <column>");
+                }
+                break;
+
             case "pick":
                 //TODO: client side checks
                 if (args.length == 1) {
@@ -51,6 +109,7 @@ public class AssembleCLIScreen extends CLIScreen{
                     getServer().pickTile(getClient(), id);
                 }
                 break;
+
             case "finish":
                 if(getLastUpdate().getCurrentGame().getCurrentGamePhaseType() != GamePhaseType.ASSEMBLE){
                     setScreenMessage("The game is not in assembly phase.");
