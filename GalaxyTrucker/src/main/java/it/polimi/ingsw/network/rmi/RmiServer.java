91156bb3e44bc02399ea49533940097977c83add
Manuel Maiuolo <manuelmaiuolo@gmail.com>
2025-04-12 01:10:52 +0200

    refactor(RmiServer): removed a lot of duplicated code by implementing subclass of RmiServer: PlayerGameInstance -> to validate client with uuid connection feat(RmiServer): RMI implemented for PLACE_TILE

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/network/rmi/RmiServer.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/network/rmi/RmiServer.java
@@ -3,6 +3,7 @@ package it.polimi.ingsw.network.rmi;
 import it.polimi.ingsw.GamesHandler;
 import it.polimi.ingsw.enums.GameLevel;
 import it.polimi.ingsw.enums.GamePhaseType;
+import it.polimi.ingsw.enums.Rotation;
 import it.polimi.ingsw.game.Game;
 import it.polimi.ingsw.game.exceptions.DrawTileException;
 import it.polimi.ingsw.game.exceptions.GameNotFoundException;
@@ -23,8 +24,11 @@ import it.polimi.ingsw.playerInput.exceptions.WrongPlayerTurnException;
 import it.polimi.ingsw.shipboard.LoadableType;
 import it.polimi.ingsw.shipboard.ShipBoard;
 import it.polimi.ingsw.shipboard.exceptions.AlreadyEndedAssemblyException;
+import it.polimi.ingsw.shipboard.exceptions.OutOfBuildingAreaException;
 import it.polimi.ingsw.shipboard.exceptions.ThatTileIdDoesNotExistsException;
+import it.polimi.ingsw.shipboard.exceptions.TileAlreadyPresentException;
 import it.polimi.ingsw.shipboard.tiles.TileSkeleton;
+import it.polimi.ingsw.shipboard.tiles.exceptions.FixedTileException;
 import it.polimi.ingsw.shipboard.tiles.exceptions.NotEnoughItemsException;
 import it.polimi.ingsw.shipboard.tiles.exceptions.TooMuchLoadException;
 import it.polimi.ingsw.shipboard.tiles.exceptions.UnsupportedLoadableItemException;
@@ -34,6 +38,29 @@ import java.rmi.RemoteException;
 import java.util.*;
 
 public class RmiServer implements IServer {
+
+	private static class PlayerGameInstance {
+		UUID connectionUUID;
+		Player player;
+		Game game;
+		private PlayerGameInstance(UUID connectionUUID, Player player, Game game) {
+			this.connectionUUID = connectionUUID;
+			this.player = player;
+			this.game = game;
+		}
+		static PlayerGameInstance validateClient(GamesHandler gamesHandler, GameServer gameServer,
+													 IClient client) throws RemoteException {
+			UUID connectionUUID = gameServer.getUUIDbyConnection(client);
+			Player player = gamesHandler.getPlayerByConnection(connectionUUID);
+			Game game = gamesHandler.findGameByClientUUID(connectionUUID);
+			//Check that we have a reference to the game & player
+			if (player == null || game == null) {
+				client.updateClient(new ClientUpdate(connectionUUID, "You are not in a game."));
+				return null;
+			}
+			return new PlayerGameInstance(connectionUUID, player, game);
+		}
+	}
 
 	final GamesHandler gamesHandler;
 	final GameServer gameServer;
@@ -94,212 +121,200 @@ public class RmiServer implements IServer {
 
 	@Override
 	public void activateTiles(IClient client, Set<Coordinates> tilesToActivate) throws RemoteException {
-		UUID connectionUUID = gameServer.getUUIDbyConnection(client);
-		Player player = gamesHandler.getPlayerByConnection(connectionUUID);
-		Game game = gamesHandler.findGameByClientUUID(connectionUUID);
-		//Check that we have a reference to the game & player
-		if(player == null || game == null) {
-			client.updateClient(new ClientUpdate(connectionUUID, "You are not in a game."));
-			return;
-		}
-		//Actually try to perform the action
+		PlayerGameInstance pg = PlayerGameInstance.validateClient(gamesHandler, gameServer, client);
+		if (pg == null) return;
+		// else: actually try to perform the action
+
 		try {
-			game.getGameData().getPIRHandler().getActivateTiles().activateTiles(player, tilesToActivate);
-			client.updateClient(new ClientUpdate(connectionUUID));
+			pg.game.getGameData().getPIRHandler().getActivateTiles().activateTiles(pg.player, tilesToActivate);
+			client.updateClient(new ClientUpdate(pg.connectionUUID));
 		} catch (WrongPlayerTurnException | InputNotSupportedException | NotEnoughItemsException |
 				 TileNotAvailableException e) {
-			client.updateClient(new ClientUpdate(connectionUUID, e.getMessage()));
+			client.updateClient(new ClientUpdate(pg.connectionUUID, e.getMessage()));
 		}
-
-
 	}
 
 	@Override
 	public void allocateLoadables(IClient client, Map<Coordinates, List<LoadableType>> cargoToAdd) throws RemoteException {
-		UUID connectionUUID = gameServer.getUUIDbyConnection(client);
-		Player player = gamesHandler.getPlayerByConnection(connectionUUID);
-		Game game = gamesHandler.findGameByClientUUID(connectionUUID);
-		if(player == null || game == null){
-			client.updateClient(new ClientUpdate(connectionUUID, "You are not in a game."));
-			return;
-		}
+		PlayerGameInstance pg = PlayerGameInstance.validateClient(gamesHandler, gameServer, client);
+		if (pg == null) return;
+		// else: actually try to perform the action
 
 		try {
-			game.getGameData().getPIRHandler().getAddLoadables().addLoadables(player, cargoToAdd);
-			client.updateClient(new ClientUpdate(connectionUUID));
+			pg.game.getGameData().getPIRHandler().getAddLoadables().addLoadables(pg.player, cargoToAdd);
+			client.updateClient(new ClientUpdate(pg.connectionUUID));
 		} catch (InputNotSupportedException | WrongPlayerTurnException | TileNotAvailableException |
 				 UnsupportedLoadableItemException | TooMuchLoadException e) {
-			client.updateClient(new ClientUpdate(connectionUUID, e.getMessage()));
+			client.updateClient(new ClientUpdate(pg.connectionUUID, e.getMessage()));
 		}
 	}
 
 	@Override
 	public void forceEndTurn(IClient client) throws RemoteException {
-		UUID connectionUUID = gameServer.getUUIDbyConnection(client);
-		Player player = gamesHandler.getPlayerByConnection(connectionUUID);
-		Game game = gamesHandler.findGameByClientUUID(connectionUUID);
-		if(player == null || game == null){
-			//TODO: player or game not found.
-			return;
-		}
+		PlayerGameInstance pg = PlayerGameInstance.validateClient(gamesHandler, gameServer, client);
+		if (pg == null) return;
+		// else: actually try to perform the action
+
 		try {
-			game.getGameData().getPIRHandler().endTurn(player);
+			pg.game.getGameData().getPIRHandler().endTurn(pg.player);
 		} catch (WrongPlayerTurnException e) {
-			client.updateClient(new ClientUpdate(connectionUUID, e.getMessage()));
+			client.updateClient(new ClientUpdate(pg.connectionUUID, e.getMessage()));
 		}
-		client.updateClient(new ClientUpdate(connectionUUID));
+		client.updateClient(new ClientUpdate(pg.connectionUUID));
 	}
 
 	@Override
 	public void updateGameSettings(IClient client, GameLevel level, int minPlayers) throws RemoteException {
-		UUID connectionUUID = gameServer.getUUIDbyConnection(client);
-		Player player = gamesHandler.getPlayerByConnection(connectionUUID);
-		Game game = gamesHandler.findGameByClientUUID(connectionUUID);
-		if(game == null || player == null){
-			client.updateClient(new ClientUpdate(connectionUUID, "You are not in a game."));
-			return;
-		}
-		if(!Objects.equals(game.getGameData().getGameLeader(), player.getUsername())){
-			client.updateClient(new ClientUpdate(connectionUUID, "You must be the game leader to perform this."));
+		PlayerGameInstance pg = PlayerGameInstance.validateClient(gamesHandler, gameServer, client);
+		if (pg == null) return;
+		// else: actually try to perform the action
+
+		if(!Objects.equals(pg.game.getGameData().getGameLeader(), pg.player.getUsername())){
+			client.updateClient(new ClientUpdate(pg.connectionUUID, "You must be the game leader to perform this."));
 			return;
 		}
 
-		game.getGameData().setLevel(level);
-		game.getGameData().setRequiredPlayers(minPlayers);
-		GameServer.getInstance().broadcastUpdate(game);
+		pg.game.getGameData().setLevel(level);
+		pg.game.getGameData().setRequiredPlayers(minPlayers);
+		GameServer.getInstance().broadcastUpdate(pg.game);
 	}
 
 	// ASSEMBLE PHASE
 
 	@Override
 	public void flipHourglass(IClient client) throws RemoteException {
-		UUID connectionUUID = gameServer.getUUIDbyConnection(client);
-		Game game = gamesHandler.findGameByClientUUID(connectionUUID);
-		Player player = gamesHandler.getPlayerByConnection(connectionUUID);
-		if(game == null | player == null){
-			client.updateClient(new ClientUpdate(connectionUUID, "You are not in a game."));
-			return;
-		}
+		PlayerGameInstance pg = PlayerGameInstance.validateClient(gamesHandler, gameServer, client);
+		if (pg == null) return;
+		// else: actually try to perform the action
 
-		if (game.getGameData().getCurrentGamePhaseType().equals(GamePhaseType.ASSEMBLE)){
+		if (pg.game.getGameData().getCurrentGamePhaseType().equals(GamePhaseType.ASSEMBLE)){
 			try {
-				game.getGameData().getCurrentGamePhase().startTimer(player);
+				pg.game.getGameData().getCurrentGamePhase().startTimer(pg.player);
 			} catch (TimerIsAlreadyRunningException | CommandNotAllowedException e) {
-				client.updateClient(new ClientUpdate(connectionUUID, e.getMessage()));
+				client.updateClient(new ClientUpdate(pg.connectionUUID, e.getMessage()));
 				return;
 			}
 		}
-		GameServer.getInstance().broadcastUpdate(game);
+		GameServer.getInstance().broadcastUpdate(pg.game);
 	}
 
 	@Override
 	public void drawTile(IClient client) throws RemoteException {
-		UUID connectionUUID = gameServer.getUUIDbyConnection(client);
-		Player player = gamesHandler.getPlayerByConnection(connectionUUID);
-		Game game = gamesHandler.findGameByClientUUID(connectionUUID);
-
-		if(player == null || game == null){
-			client.updateClient(new ClientUpdate(connectionUUID, "You are not in a game."));
-			return;
-		}
+		PlayerGameInstance pg = PlayerGameInstance.validateClient(gamesHandler, gameServer, client);
+		if (pg == null) return;
+		// else: actually try to perform the action
 
 		try {
-			player.drawTile(game.getGameData());
+			pg.player.drawTile(pg.game.getGameData());
 		} catch (DrawTileException | AlreadyHaveTileInHandException e) {
-			client.updateClient(new ClientUpdate(connectionUUID, e.getMessage()));
+			client.updateClient(new ClientUpdate(pg.connectionUUID, e.getMessage()));
 			return;
 		}
 
-		GameServer.getInstance().broadcastUpdate(game);
+		GameServer.getInstance().broadcastUpdate(pg.game);
 	}
 
 	@Override
 	public void discardTile(IClient client) throws RemoteException {
-		UUID connectionUUID = gameServer.getUUIDbyConnection(client);
-		Player player = gamesHandler.getPlayerByConnection(connectionUUID);
-		Game game = gamesHandler.findGameByClientUUID(connectionUUID);
-
-		if (player == null || game == null) {
-			client.updateClient(new ClientUpdate(connectionUUID, "You are not in a game."));
-			return;
-		}
+		PlayerGameInstance pg = PlayerGameInstance.validateClient(gamesHandler, gameServer, client);
+		if (pg == null) return;
+		// else: actually try to perform the action
 
 		try {
-			player.discardTile(game.getGameData());
+			pg.player.discardTile(pg.game.getGameData());
 		} catch (NoTileInHandException e) {
-			client.updateClient(new ClientUpdate(connectionUUID, e.getMessage()));
+			client.updateClient(new ClientUpdate(pg.connectionUUID, e.getMessage()));
 			return;
 		}
 
-		GameServer.getInstance().broadcastUpdate(game);
+		GameServer.getInstance().broadcastUpdate(pg.game);
 	}
 
 	@Override
 	public void reserveTile(IClient client) throws RemoteException {
-		UUID connectionUUID = gameServer.getUUIDbyConnection(client);
-		Player player = gamesHandler.getPlayerByConnection(connectionUUID);
-		Game game = gamesHandler.findGameByClientUUID(connectionUUID);
-
-		if (player == null || game == null) {
-			client.updateClient(new ClientUpdate(connectionUUID, "You are not in a game."));
-			return;
-		}
+		PlayerGameInstance pg = PlayerGameInstance.validateClient(gamesHandler, gameServer, client);
+		if (pg == null) return;
+		// else: actually try to perform the action
 
 		try {
-			player.setReservedTiles(player.getTileInHand());
-			player.discardTile(game.getGameData());
+			pg.player.setReservedTiles(pg.player.getTileInHand());
+			pg.player.discardTile(pg.game.getGameData());
 		}catch (NoTileInHandException | TooManyReservedTilesException e){
-			client.updateClient(new ClientUpdate(connectionUUID, e.getMessage()));
+			client.updateClient(new ClientUpdate(pg.connectionUUID, e.getMessage()));
 			return;
 		}
 
-		GameServer.getInstance().broadcastUpdate(game);
+		GameServer.getInstance().broadcastUpdate(pg.game);
 
 	}
 
 	@Override
 	public void pickTile(IClient client, Integer id) throws RemoteException {
-		UUID connectionUUID = gameServer.getUUIDbyConnection(client);
-		Player player = gamesHandler.getPlayerByConnection(connectionUUID);
-		Game game = gamesHandler.findGameByClientUUID(connectionUUID);
-
-		if (player == null || game == null) {
-			client.updateClient(new ClientUpdate(connectionUUID, "You are not in a game."));
-			return;
-		}
+		PlayerGameInstance pg = PlayerGameInstance.validateClient(gamesHandler, gameServer, client);
+		if (pg == null) return;
+		// else: actually try to perform the action
 
 		try{
-			player.pickTile(game.getGameData(), id);
+			pg.player.pickTile(pg.game.getGameData(), id);
 		} catch (AlreadyHaveTileInHandException | ThatTileIdDoesNotExistsException e) {
-			client.updateClient(new ClientUpdate(connectionUUID, e.getMessage()));
+			client.updateClient(new ClientUpdate(pg.connectionUUID, e.getMessage()));
 			return;
         }
 
-		GameServer.getInstance().broadcastUpdate(game);
+		GameServer.getInstance().broadcastUpdate(pg.game);
+    }
+
+	@Override
+	public void placeTile(IClient client, Coordinates coordinates, Rotation rotation) throws RemoteException {
+		PlayerGameInstance pg = PlayerGameInstance.validateClient(gamesHandler, gameServer, client);
+		if (pg == null) return;
+		// else: actually try to perform the action
+
+		TileSkeleton tileInHand = pg.player.getTileInHand();
+		if (tileInHand == null) {
+			client.updateClient(new ClientUpdate(pg.connectionUUID, "You don't have a tile in hand."));
+			return;
+		}
+
+		ShipBoard shipBoard = pg.player.getShipBoard();
+		if (shipBoard == null) {
+			client.updateClient(new ClientUpdate(pg.connectionUUID, "You don't have a shipboard."));
+			return;
+		}
+
+		try {
+			tileInHand.resetRotation();  // ensure there is no unhandled previous rotation
+			tileInHand.rotateTile(rotation);  // apply the desired rotation
+			shipBoard.setTile(tileInHand, coordinates);  // place the tile
+			// if here, the tile has been correctly placed:
+			pg.player.setTileInHand(null);  // remove tile from hand
+		} catch (FixedTileException | TileAlreadyPresentException | OutOfBuildingAreaException
+				 | AlreadyHaveTileInHandException e) {
+			client.updateClient(new ClientUpdate(pg.connectionUUID, e.getMessage()));
+			return;
+        }
+
+        GameServer.getInstance().broadcastUpdate(pg.game);
     }
 
 	@Override
 	public void finishAssembling(IClient client) throws RemoteException {
-		UUID connectionUUID = gameServer.getUUIDbyConnection(client);
-		Player player = gamesHandler.getPlayerByConnection(connectionUUID);
-		Game game = gamesHandler.findGameByClientUUID(connectionUUID);
-		if (player == null || game == null) {
-			client.updateClient(new ClientUpdate(connectionUUID, "You are not in a game."));
-			return;
-		}
+		PlayerGameInstance pg = PlayerGameInstance.validateClient(gamesHandler, gameServer, client);
+		if (pg == null) return;
+		// else: actually try to perform the action
 
-		ShipBoard shipBoard = player.getShipBoard();
+		ShipBoard shipBoard = pg.player.getShipBoard();
 
 		if(shipBoard == null){
-			client.updateClient(new ClientUpdate(connectionUUID, "You don't have a shipboard."));
+			client.updateClient(new ClientUpdate(pg.connectionUUID, "You don't have a shipboard."));
 			//This means that something went wrong or the player is in pre-assembly or lobby phase.
 			return;
 		}
 
 		try {
-			shipBoard.endAssembly(game.getGameData());
+			shipBoard.endAssembly(pg.game.getGameData());
 		} catch (AlreadyEndedAssemblyException | CommandNotAllowedException e) {
-			client.updateClient(new ClientUpdate(connectionUUID, e.getMessage()));
+			client.updateClient(new ClientUpdate(pg.connectionUUID, e.getMessage()));
 		}
 	}
 }
