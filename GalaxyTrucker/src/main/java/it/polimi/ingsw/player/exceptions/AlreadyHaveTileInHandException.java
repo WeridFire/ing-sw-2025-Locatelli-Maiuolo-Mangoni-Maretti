2e07c1811c431a672c6a14ba780ce7e579dbac2f
Manuel Maiuolo <manuelmaiuolo@gmail.com>
2025-04-13 22:08:39 +0200

    feat: new Exceptions and constructors of existing ones

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/player/exceptions/AlreadyHaveTileInHandException.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/player/exceptions/AlreadyHaveTileInHandException.java
@@ -4,4 +4,7 @@ public class AlreadyHaveTileInHandException extends Exception {
 	public AlreadyHaveTileInHandException(String s) {
 		super(s);
 	}
+	public AlreadyHaveTileInHandException() {
+		this("You already are holding a tile.");
+	}
 }
--- GalaxyTrucker/src/main/java/it/polimi/ingsw/player/exceptions/TooManyReservedTilesException.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/player/exceptions/TooManyReservedTilesException.java
@@ -1,4 +1,7 @@
 package it.polimi.ingsw.player.exceptions;
 
 public class TooManyReservedTilesException extends Exception {
+    public TooManyReservedTilesException() {
+        super("You already have the maximum number of reserved tiles.");
+    }
 }
