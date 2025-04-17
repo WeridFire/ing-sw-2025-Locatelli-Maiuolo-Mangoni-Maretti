82a3ea3037ed683c361e48d6a6f2c3a176830f0f
Manuel Maiuolo <manuelmaiuolo@gmail.com>
2025-04-17 17:50:50 +0200

    feat(GameLevel): GameLevel.fromInteger to parse int into GameLevel

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/enums/GameLevel.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/enums/GameLevel.java
@@ -3,5 +3,13 @@ package it.polimi.ingsw.enums;
 import java.io.Serializable;
 
 public enum GameLevel implements Serializable {
-    TESTFLIGHT, ONE, TWO
+    TESTFLIGHT, ONE, TWO;
+
+    public static GameLevel fromInteger(int i) {
+        return switch (i) {
+            case 1 -> ONE;
+            case 2 -> TWO;
+            default -> TESTFLIGHT;
+        };
+    }
 }
