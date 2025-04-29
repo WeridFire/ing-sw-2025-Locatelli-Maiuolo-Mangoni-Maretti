84a03df345950f5647c72b5b01e086ad56c649c2
Manuel Maiuolo <manuelmaiuolo@gmail.com>
2025-04-29 17:55:51 +0200

    feat!: implemented javaFX in MainApp
    BREAKING CHANGE: can now work on GUI

---
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/MainApp.java
@@ -0,0 +1,23 @@
+package it.polimi.ingsw;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello JavaFX");
+        StackPane root = new StackPane();
+        root.getChildren().add(new Label("Hello, JavaFX!"));
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
