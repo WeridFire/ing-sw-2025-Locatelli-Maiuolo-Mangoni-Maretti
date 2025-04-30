898140934e622d3b11ea110de294f7c9742caa1c
Manuel Maiuolo <manuelmaiuolo@gmail.com>
2025-04-30 14:10:11 +0200

    feat(network): new exception AlreadyRunningServerException when attempting to start a server multiple times

---
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/network/exceptions/AlreadyRunningServerException.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.network.exceptions;
+
+public class AlreadyRunningServerException extends Exception {
+    public AlreadyRunningServerException(String message) {
+        super(message);
+    }
+}
