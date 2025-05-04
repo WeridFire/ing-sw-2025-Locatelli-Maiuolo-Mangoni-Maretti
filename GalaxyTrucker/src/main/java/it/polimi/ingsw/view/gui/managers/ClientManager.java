19789503e17029b6c32512ff70b556a723f74b5d
Maretti Filippo <69721649+WeridFire@users.noreply.github.com>
2025-05-04 12:10:41 +0200

    feat(GUI): Structure improved

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/view/gui/managers/ClientManager.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/view/gui/managers/ClientManager.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.view.gui.managers;
 
 import it.polimi.ingsw.network.GameClient;
+import it.polimi.ingsw.view.cli.CLIScreenHandler;
 import it.polimi.ingsw.view.gui.UIs.LobbyUI;
 import it.polimi.ingsw.view.gui.utils.AlertUtils;
 import it.polimi.ingsw.view.gui.UIs.LoginUI;
@@ -11,6 +12,7 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.rmi.RemoteException;
 import java.util.function.Consumer;
 
 public class ClientManager {
@@ -19,7 +21,7 @@ public class ClientManager {
     private final Consumer<Node> sceneUpdater;
     private final Runnable showLauncherCallback;
 
-    private GameClient client;
+    private GameClient gameClient;
     private String username;
 
     public ClientManager(Stage primaryStage, Consumer<Node> sceneUpdater, Runnable showLauncherCallback) {
@@ -58,8 +60,7 @@ public class ClientManager {
         int RMI_PORT = 1111;
 
         try {
-
-            this.client = new GameClient(useRmi,
+            this.gameClient = new GameClient(useRmi,
                     HOST,
                     useRmi ? RMI_PORT : SOCKET_PORT
             );
@@ -75,7 +76,7 @@ public class ClientManager {
     private void createOrJoinGame(String username) {
 
         Button createButton = new Button("Create Game");
-        //createButton.setOnAction(_ -> clientManager.showLoginUI());
+        createButton.setOnAction(_ -> handleCreateGame(username));
 
         Button joinButton = new Button("Join Game");
         //joinButton.setOnAction(_ -> clientManager.showLoginUI());
@@ -99,7 +100,11 @@ public class ClientManager {
 
     public void handleCreateGame(String username) {
         //create the actual game
-
+        try {
+            gameClient.getServer().createGame(gameClient, username);
+        } catch (RemoteException e) {
+            throw new RuntimeException(e);
+        }
 
         //change screen
         sceneUpdater.accept(new LobbyUI(username).getLayout());
