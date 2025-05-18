3fac823155234de6af5d6a892e5a18f8a2248fe0
AdriCman <adrianoclaudio.mangoni@mail.polimi.it>
2025-05-18 23:47:36 +0200

    feat(WarPunish,ent) : implemented method to apply projectile punishment

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/cards/warzone/WarPunishmentProjectile.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/cards/warzone/WarPunishmentProjectile.java
@@ -3,6 +3,10 @@ package it.polimi.ingsw.cards.warzone;
 import it.polimi.ingsw.cards.projectile.Projectile;
 import it.polimi.ingsw.game.GameData;
 import it.polimi.ingsw.player.Player;
+import it.polimi.ingsw.playerInput.PIRUtils;
+import it.polimi.ingsw.playerInput.PIRs.PIRMultipleChoice;
+import it.polimi.ingsw.shipboard.exceptions.NoTileFoundException;
+import it.polimi.ingsw.shipboard.exceptions.OutOfBuildingAreaException;
 
 public class WarPunishmentProjectile implements WarPunishment {
 
@@ -14,6 +18,23 @@ public class WarPunishmentProjectile implements WarPunishment {
 
     @Override
     public void apply(Player player, GameData gameData) {
-        // TODO: implement hitting the player accordingly to the projectiles.
+        for(Projectile proj : projectiles){
+
+            game.getPIRHandler().setAndRunTurn(new PIRMultipleChoice(
+                    player,
+                    30,
+                    "Do you want to roll the dice?",
+                    new String[]{"Yes"},
+                    0
+            ));
+
+            proj.roll2D6();
+            boolean defended = PIRUtils.runPlayerProjectileDefendRequest(player, proj, gameData);
+            if(!defended){
+                try {
+                    player.getShipBoard().hit(proj.getDirection(), proj.getCoord());
+                } catch (NoTileFoundException | OutOfBuildingAreaException e) {
+                    throw new RuntimeException(e);  // should never happen -> runtime exception
+        }
     }
 }
