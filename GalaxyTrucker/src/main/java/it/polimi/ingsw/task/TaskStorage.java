1c78081db967b8dc4e8564c2a19137c616d3c3b0
Dere-Wah <derexcontact@gmail.com>
2025-05-24 23:05:34 +0200

    docs

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/task/TaskStorage.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/task/TaskStorage.java
@@ -12,19 +12,30 @@ public class TaskStorage {
 
 	private final UUID gameId;
 
+	/**
+	 * Storage for containing all the pending and past tasks of a game.
+	 * @param gameId The game associated to the task storage.
+	 */
 	public TaskStorage(UUID gameId) {
 		this.previousTasks = new ArrayList<Task>();
 		this.pendingTasks = new ArrayList<Task>();
 		this.gameId = gameId;
 	}
 
-
+	/**
+	 * Adds a new task to the pending tasks queue.
+	 * @param task the task to be added.
+	 */
 	public void addTask(Task task){
 		task.setGameId(gameId);
 		pendingTasks.add(task);
 	}
 
-
+	/**
+	 * Functions that gets called periodically by the main thread.
+	 * Iterates through each pending task in the game and checks for it completion, calling the
+	 * finish function and removing the current task.
+	 */
 	public void checkTasks(){
 		synchronized(pendingTasks){
 			pendingTasks.forEach((task)->{
