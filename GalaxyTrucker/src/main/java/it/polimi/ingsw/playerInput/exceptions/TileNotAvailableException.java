4bb17958a7a42ab2255c350adeec68b966a5b88d
Dere-Wah <derexcontact@gmail.com>
2025-05-24 21:56:22 +0200

    add new signature for task system

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/playerInput/exceptions/TileNotAvailableException.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/playerInput/exceptions/TileNotAvailableException.java
@@ -1,10 +1,15 @@
 package it.polimi.ingsw.playerInput.exceptions;
 
 import it.polimi.ingsw.playerInput.PIRType;
+import it.polimi.ingsw.task.TaskType;
 import it.polimi.ingsw.util.Coordinates;
 
 public class TileNotAvailableException extends Exception {
 	public TileNotAvailableException(Coordinates coordinate, PIRType turnType) {
 		super("The tile at coordinates " + coordinate.toString() + " is not supported for the action " + turnType);
+	}
+
+	public TileNotAvailableException(Coordinates coordinate, TaskType taskType) {
+		super("The tile at coordinates " + coordinate.toString() + " is not supported for the task " + taskType);
 	}
 }
