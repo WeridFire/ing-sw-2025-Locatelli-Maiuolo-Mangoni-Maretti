1e999e7405fc5012ae9626b0f5e8201be7ba5a82
AdriCman <adrianoclaudio.mangoni@mail.polimi.it>
2025-05-29 23:51:02 +0200

    fix(OpenSpaceCard) : adapted card to new program logic

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/cards/OpenSpaceCard.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/cards/OpenSpaceCard.java
@@ -1,19 +1,22 @@
 package it.polimi.ingsw.cards;
 
-import it.polimi.ingsw.GamesHandler;
 import it.polimi.ingsw.enums.AnchorPoint;
 import it.polimi.ingsw.enums.Direction;
 import it.polimi.ingsw.enums.PowerType;
-import it.polimi.ingsw.game.Game;
 import it.polimi.ingsw.game.GameData;
 import it.polimi.ingsw.player.Player;
 import it.polimi.ingsw.playerInput.PIRUtils;
+import it.polimi.ingsw.playerInput.exceptions.TileNotAvailableException;
+import it.polimi.ingsw.playerInput.exceptions.WrongPlayerTurnException;
+import it.polimi.ingsw.shipboard.LoadableType;
+import it.polimi.ingsw.shipboard.tiles.exceptions.NotEnoughItemsException;
+import it.polimi.ingsw.shipboard.visitors.VisitorCalculatePowers;
+import it.polimi.ingsw.task.customTasks.TaskActivateTiles;
+import it.polimi.ingsw.task.customTasks.TaskRemoveLoadables;
 import it.polimi.ingsw.view.cli.ANSI;
 import it.polimi.ingsw.view.cli.CLIFrame;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 import static it.polimi.ingsw.view.cli.CLIScreen.getScreenFrame;
 
@@ -28,10 +31,11 @@ public class OpenSpaceCard extends Card{
 		super("OPEN SPACE", textureName, level);
 	}
 
+
 	/**
 	 * Iterates each player, requires how many thrust power they wanna use, and moves accordingly.
 	 */
-	@Override
+	//@Override
 	public void playEffect(GameData game) {
 		for(Player p : game.getPlayersInFlight()){
 			// here we just cast to int, but know for sure that the thrusters tiles won't return numbers with decimals.
@@ -43,6 +47,72 @@ public class OpenSpaceCard extends Card{
 				game.movePlayerForward(p, steps);
 			}
 		}
+	}
+
+	@Override
+	public void startCardBehaviour(GameData game) {
+		playTask(game,  game.getPlayersInFlight().getFirst());
+	}
+
+	public void playTask(GameData game, Player player) {
+		if(player == null){
+			//The previous player was the last one. We end the gamephase.
+			game.getCurrentGamePhase().endPhase();
+			return;
+		}
+
+		TaskActivateTiles task = new TaskActivateTiles(
+				player.getUsername(),
+				30,
+				PowerType.THRUST,
+				(p, coordinatesSet) -> {
+				}
+		);
+
+		task.setOnFinish((p, coordinatesSet) -> {
+			//After player has chosen which tiles to activate
+			try {
+
+				//activate tiles
+				task.activateTiles(p, coordinatesSet);
+
+				//remove batteries
+				game.getTaskStorage().addTask(new TaskRemoveLoadables(
+						p,
+						30,
+						Set.of(LoadableType.BATTERY),
+						coordinatesSet.size(),
+						(p1) -> {
+
+						}
+				));
+
+				//calculate total power (previously done by PIRs)
+				//moved it here since facing an enemy is the only instance of the game
+				//where it's needed to calculate the thrust
+				VisitorCalculatePowers.CalculatorPowerInfo powerInfo = player.getShipBoard().getVisitorCalculatePowers().getInfoPower(PowerType.THRUST);
+
+				//activated firepower
+				int totalThrustPower = (int) powerInfo.getLocationsToActivate().entrySet().stream()
+						.filter(entry -> coordinatesSet.contains(entry.getKey()))
+						.mapToDouble(Map.Entry::getValue)
+						.sum();
+
+				//add base firepower
+				totalThrustPower += (int) powerInfo.getBasePower();
+
+				//add bonus if purple alien is present
+				totalThrustPower += (int) powerInfo.getBonus(totalThrustPower);
+
+				game.movePlayerForward(player, totalThrustPower);
+
+			} catch (WrongPlayerTurnException | NotEnoughItemsException | TileNotAvailableException e) {
+				throw new RuntimeException(e);
+			}
+		});
+
+		game.getTaskStorage().addTask(task);
+		playTask(game, game.getNextPlayerInFlight(player));
 	}
 
 	/**
