3cb0630501188ea9f745d147c323a701078fe221
Dere-Wah <derexcontact@gmail.com>
2025-06-05 22:47:08 +0200

    feat(IShipIntegrityListener): add new parameter in update method to pass onto the future game logic

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/shipboard/integrity/IShipIntegrityListener.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/shipboard/integrity/IShipIntegrityListener.java
@@ -1,7 +1,10 @@
 package it.polimi.ingsw.shipboard.integrity;
 
+import it.polimi.ingsw.player.Player;
+
 import java.io.Serializable;
+import java.util.function.Consumer;
 
 public interface IShipIntegrityListener extends Serializable {
-    void update(IntegrityProblem integrityProblem);
+    void update(IntegrityProblem integrityProblem, Consumer<Player> postCheck);
 }
