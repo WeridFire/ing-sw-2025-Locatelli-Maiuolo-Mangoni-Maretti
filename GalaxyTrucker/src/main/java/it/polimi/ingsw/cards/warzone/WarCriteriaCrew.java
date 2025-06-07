a07c6622ffb6c590052aa99f8fae3085c7c5d52f
Dere-Wah <derexcontact@gmail.com>
2025-06-07 13:05:26 +0200

    compute criteria for calculating crews.

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/cards/warzone/WarCriteriaCrew.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/cards/warzone/WarCriteriaCrew.java
@@ -1,7 +1,10 @@
 package it.polimi.ingsw.cards.warzone;
 
+import it.polimi.ingsw.game.GameData;
 import it.polimi.ingsw.player.Player;
 import it.polimi.ingsw.shipboard.LoadableType;
+
+import java.util.function.Consumer;
 
 public class WarCriteriaCrew implements WarCriteria {
     
@@ -10,5 +13,12 @@ public class WarCriteriaCrew implements WarCriteria {
         int crew1 = p1.getShipBoard().getVisitorCalculateCargoInfo().getCrewInfo().countAll(LoadableType.CREW_SET);
         int crew2 = p2.getShipBoard().getVisitorCalculateCargoInfo().getCrewInfo().countAll(LoadableType.CREW_SET);
         return Integer.compare(crew1, crew2);
+    }
+
+    @Override
+    public void computeCriteria(GameData game, Consumer<Player> postCompute) {
+        Player p = game.getPlayersInFlight().stream()
+                .min(this).orElse(null);
+        postCompute.accept(p);
     }
 }
