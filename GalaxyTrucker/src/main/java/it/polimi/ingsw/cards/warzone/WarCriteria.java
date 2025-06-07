22c19989c78c80d889e8ef3b32fb3e4ec1461001
Dere-Wah <derexcontact@gmail.com>
2025-06-07 13:04:36 +0200

    update WarCriteria to accept consumer for executing actions after that a criteria has been computed

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/cards/warzone/WarCriteria.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/cards/warzone/WarCriteria.java
@@ -2,6 +2,13 @@ package it.polimi.ingsw.cards.warzone;
 
 import java.io.Serializable;
 import java.util.Comparator;
+import java.util.function.Consumer;
+
+import it.polimi.ingsw.game.GameData;
 import it.polimi.ingsw.player.Player;
 
-public interface WarCriteria extends Comparator<Player>, Serializable { }
+public interface WarCriteria extends Comparator<Player>, Serializable {
+
+	 void computeCriteria(GameData game, Consumer<Player> postCompute);
+
+}
