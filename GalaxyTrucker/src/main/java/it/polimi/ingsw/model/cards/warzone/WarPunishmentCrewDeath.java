557a9a8e78dbc0e9c31efa9a89972a8cfa2b38fa
AdriCman <adrianoclaudio.mangoni@mail.polimi.it>
2025-06-27 22:57:34 +0200

    fix(PunishmentCrewDeath) now lets the player decide which crew to lose

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/model/cards/warzone/WarPunishmentCrewDeath.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/model/cards/warzone/WarPunishmentCrewDeath.java
@@ -2,7 +2,13 @@ package it.polimi.ingsw.model.cards.warzone;
 
 import it.polimi.ingsw.model.game.GameData;
 import it.polimi.ingsw.model.player.Player;
+import it.polimi.ingsw.model.playerInput.PIRs.PIRDelay;
+import it.polimi.ingsw.model.playerInput.PIRs.PIRRemoveLoadables;
+import it.polimi.ingsw.model.shipboard.LoadableType;
+import it.polimi.ingsw.util.Default;
 import it.polimi.ingsw.view.cli.ANSI;
+
+import java.util.Set;
 
 public class WarPunishmentCrewDeath implements WarPunishment {
 
@@ -19,7 +25,16 @@ public class WarPunishmentCrewDeath implements WarPunishment {
 
     @Override
     public void apply(Player player, GameData gameData) {
-        // TODO: implement method so that it asks the player where they wants their crew to be removed.
-        player.getShipBoard().loseCrew(crewAmount);
+
+        PIRDelay pirDelay = new PIRDelay(player, Default.PIR_SHORT_SECONDS,
+                "You lost in the WarZone! You need to remove" + crewAmount + "of your crew from the ship", null
+                );
+        gameData.getPIRHandler().setAndRunTurn(
+                pirDelay
+        );
+        PIRRemoveLoadables pirRemoveLoadables = new PIRRemoveLoadables(player, Default.PIR_SECONDS, LoadableType.CREW_SET, crewAmount);
+        gameData.getPIRHandler().setAndRunTurn(
+                pirRemoveLoadables
+        );
     }
 }
